1a52dddbc6e41b8c8db0e6be719602a53f9180b7
shunjikonishi <shun.konishi@nifty.com>
2015-04-16 21:01:38 +0900

    Add GitHubAPI

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
+import codecheck.github.api.GitHubAPI
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  val api = new GitHubAPI(Play.configuration.getString("github.token").get)
+
+  def index = Action.async {
+    api.getOrganization("code-check").map { org =>
+      Ok(org.toString)
+    }
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -4,11 +4,20 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.6"
 
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
   cache,
-  ws
+  ws,
+  "jp.co.givery" %% "github-api" % "0.1.0"
+)
+
+sources in (Compile, doc) := Seq.empty
+
+publishArtifact in (Compile, packageDoc) := false
+
+resolvers ++= Seq(
+  "Givery SBT Repository on Github" at "http://givery-technology.github.io/sbt-repo/"
 )
